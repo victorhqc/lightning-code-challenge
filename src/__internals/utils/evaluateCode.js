b441475706adf141947e4f2b1d7d644642939b92
Victor Quiroz <victorhqc@gmail.com>
2018-04-10 17:58:23 +0200

    Create custom test suite

--- src/__internals/utils/evaluateCode.js
+++ src/__internals/utils/evaluateCode.js
@@ -1,92 +1,54 @@
+import sumBy from 'lodash/sumBy';
+
 export const expect = value => ({
   toBe: (expected) => {
     if (value !== expected) {
-      return {
-        isFailed: true,
-        error: `expected ${expected} to be ${value}`,
-      };
+      throw new Error(`expected \`${expected}\` to be \`${value}\``);
     }
 
-    return {
-      isFailed: false,
-    };
+    return true;
+  },
+
+  toBeTruthy: () => {
+    if (!value) {
+      throw new Error(`expected \`${value}\` to be truthy`);
+    }
+
+    return true;
   },
 });
 
-const it = (name, callback) => {
-
-};
-
-const describe = (name, callback) => {
-  const result = callback();
-
-  return {
-    ...result,
-    name,
-  };
-};
-
-const TESTS_V2 = des
-
-const TESTS = [
-  {
-    id: 'foo',
-    functionName: 'test',
-    title: 'Should do foo',
-    check: (callback) => {
-      const result = callback();
-
-      return {
-        didTestPass: result === 'foo',
-        result,
-      };
-    },
-  },
-  {
-    id: 'pushq',
-    functionName: 'test',
-    title: 'Should do pushq',
-    check: (callback) => {
-      const result = callback();
-
-      return {
-        didTestPass: result === 'pushq',
-        result,
-      };
-    },
-  },
-];
-
-const assertTest = (test, code) => {
-
-};
-
-const evaluateCode = code => TESTS.reduce(
-  (prev, test) => {
-    const result = test.check(code[test.functionName]);
-    const totalTests = prev.totalTests + 1;
-    const testsPassed = result.didTestPass ? prev.testsPassed + 1 : prev.testsPassed;
-    const ratio = testsPassed / totalTests;
+export const it = (name, callback) => {
+  try {
+    callback();
 
     return {
-      totalTests,
-      testsPassed,
-      ratio,
-      tests: [
-        ...prev.tests,
-        {
-          id: test.id,
-          title: test.title,
-          ...result,
-        },
-      ],
+      name,
+      error: '',
+      isFailed: false,
     };
-  },
-  {
-    totalTests: 0,
-    testsPassed: 0,
-    tests: [],
-  },
-);
+  } catch (e) {
+    return {
+      name,
+      error: e.message,
+      isFailed: true,
+    };
+  }
+};
 
-export default evaluateCode;
+export const describe = (name, tests) => {
+  const passedTests = sumBy(tests, ({ isFailed }) => {
+    if (isFailed) {
+      return 0;
+    }
+
+    return 1;
+  });
+
+  return {
+    name,
+    tests,
+    ratio: passedTests / tests.length,
+    passedTests,
+  };
+};
--- src/__internals/utils/evaluateCode.spec.js
+++ src/__internals/utils/evaluateCode.spec.js
@@ -1,12 +1,84 @@
-import { expect as evaluateExpect } from './evaluateCode';
+import {
+  expect as evaluateExpect,
+  it as evaluateIt,
+  describe as evaluateDescribe,
+} from './evaluateCode';
 
 describe('expect', () => {
   describe('.toBe', () => {
     it('Should be successful when it matches', () => {
-      const result = evaluateExpect('foo').toBe('foo');
-      expect(result).toEqual({
-        isFailed: false,
-      });
+      expect(evaluateExpect('foo').toBe('foo')).toBeTruthy();
+    });
+
+    it('Should fail when it doesn\'t match', () => {
+      expect(() => {
+        evaluateExpect('foo').toBe('bar');
+      }).toThrow('expected `bar` to be `foo`');
+    });
+  });
+
+  describe('.toBeTruthy', () => {
+    it('Should be successful when is truthy', () => {
+      expect(evaluateExpect(true).toBeTruthy()).toBeTruthy();
+    });
+
+    it('Should fail when is not truthy', () => {
+      expect(() => {
+        evaluateExpect(false).toBeTruthy();
+      }).toThrow('expected `false` to be truthy');
+    });
+  });
+});
+
+describe('it', () => {
+  it('Should pass if internal `expect` pass', () => {
+    const result = evaluateIt('some internal test', () => {
+      evaluateExpect(true).toBeTruthy();
+    });
+
+    expect(result).toEqual({
+      isFailed: false,
+      error: '',
+      name: 'some internal test',
+    });
+  });
+
+  it('Should fail if internal `expect` fails', () => {
+    expect(evaluateIt('some internal failed test', () => {
+      throw new Error('oh no!');
+    })).toEqual({
+      isFailed: true,
+      error: 'oh no!',
+      name: 'some internal failed test',
+    });
+  });
+});
+
+describe('describe', () => {
+  it('Should run a set of `it` and return its results in an array', () => {
+    expect(evaluateDescribe('something being tested', [
+      evaluateIt('something passes', () => {
+        expect(true).toBe(true);
+      }),
+      evaluateIt('something fails', () => {
+        throw new Error('oh no!');
+      }),
+    ])).toEqual({
+      name: 'something being tested',
+      tests: [
+        {
+          isFailed: false,
+          error: '',
+          name: 'something passes',
+        },
+        {
+          isFailed: true,
+          error: 'oh no!',
+          name: 'something fails',
+        },
+      ],
+      passedTests: 1,
+      ratio: 1 / 2,
     });
   });
 });
