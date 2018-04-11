1a5cc42ddba0a18f30909d0cc70845e01cc50850
Victor Quiroz <victorhqc@gmail.com>
2018-04-11 22:43:03 +0200

    Fix storage

--- src/__internals/utils/storage.js
+++ src/__internals/utils/storage.js
@@ -3,6 +3,5 @@ import storage from 'local-storage-fallback';
 export const saveToStorage = (name, value) =>
   storage.setItem(name, value);
 
-export const getFromStorage = (name) => {
+export const getFromStorage = name =>
   storage.getItem(name);
-};
