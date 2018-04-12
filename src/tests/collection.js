75ae2fb3bfe1f14a7b6586195f6867d7ca25f0d1
Victor Quiroz <victorhqc@gmail.com>
2018-04-12 15:27:52 +0200

    Add collection tests

--- src/tests/collection.js
+++ src/tests/collection.js
@@ -15,6 +15,25 @@ Write a **Collection** class that has the following methods:
 - remove: (key) removes the value assigned to that key, returns true if succeeds, false if fails.
 - forEach: (callback) the callback should be able to call _this.add, this.get, etc._
 
+### Usage example
+\`\`\`js
+const collection = new Collection({
+  name: 'test',
+  email: 'hello@world',
+});
+
+collection.add('foo', 'bar');
+
+collecion.get('foo');
+
+collection.remove('foo');
+
+collection.forEach(function(key) {
+  this.get(key);
+  this.remove(key);
+});
+\`\`\`
+
 The **Collection** has to use the prototypal approach, and the _forEach_ method
 
 feel free to use ES6 sugar that your browser supports.
@@ -32,6 +51,12 @@ export const testCases = code => describe('Foo test', () => {
     it('Should come from a Collection instance', () => {
       const collection = new Scope();
       expect(collection.constructor.name).toBe('Collection');
+    }),
+
+    it('Should not share instances', () => {
+      const collection = new Scope();
+      const otherCollection = new Scope();
+      expect(collection !== otherCollection).toBeTruthy();
     }),
 
     it('add method exists', () => {
@@ -53,6 +78,16 @@ export const testCases = code => describe('Foo test', () => {
 
       collection.add('foo', 'bar');
       expect(collection.foo).toBe('bar');
+    }),
+
+    it('Constructor should add keys in object', () => {
+      const collection = new Scope({
+        name: 'test',
+        email: 'hello@world',
+      });
+
+      expect(collection.get('name')).toBe('test');
+      expect(collection.get('email')).toBe('hello@world');
     }),
 
     it('get method exists', () => {
@@ -111,6 +146,38 @@ export const testCases = code => describe('Foo test', () => {
       expect(typeof collection.forEach !== 'undefined').toBeTruthy();
       // collection.hasOwnProperty
       expect(!Object.prototype.hasOwnProperty.call(collection, 'forEach')).toBeTruthy();
+    }),
+
+    it('Should not expose data to other instances', () => {
+      const collection = new Scope();
+      collection.add('name', 'test');
+      collection.add('email', 'hello@world');
+
+      const otherCollection = new Scope({
+        name2: 'test2',
+        email2: 'hello2@world',
+      });
+
+      expect(otherCollection.get('name')).toBe(undefined);
+      expect(otherCollection.email).toBe(undefined);
+    }),
+
+    it('forEach() should allow use own methods in callback', () => {
+      const collection = new Scope({
+        name: 'test',
+        email: 'hello@world',
+      });
+
+      expect(collection.get('name')).toBe('test');
+      expect(collection.get('email')).toBe('hello@world');
+
+      collection.forEach(function inForEach(key, value) {
+        expect(this.get(key)).toBe(value);
+        this.remove(key);
+      });
+
+      expect(collection.get('name')).toBe(undefined);
+      expect(collection.get('email')).toBe(undefined);
     }),
   ];
 });
