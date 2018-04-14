321ecbb15c3661451a81b241b3252d90adaab481
Victor Quiroz <victorhqc@gmail.com>
2018-04-14 18:02:21 +0200

    Style <SideBar /> a bit

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,6 +1,8 @@
 import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
+import { mapProps } from 'recompose';
 import styled from 'styled-components';
 import { withRouter } from 'react-router';
 import map from 'lodash/map';
@@ -8,6 +10,7 @@ import isEqual from 'lodash/isEqual';
 
 import storeActiveTest from '../store/activeTest';
 import storeTests, { testsPropTypes, testPropTypes } from '../store/tests';
+import codeResults, { resultsProps } from '../store/codeResults';
 
 import {
   getColor,
@@ -22,6 +25,13 @@ import {
 
 import Container from '../atoms/Container';
 import Link from '../atoms/Link';
+import List from '../atoms/List';
+
+import RatioAsEmojis from './RatioAsEmojis';
+
+const localPropTypes = {
+  locationIn: PropTypes.oneOf(['home', 'instructions', 'test']).isRequired,
+};
 
 const Nav = styled.nav`
 
@@ -36,53 +46,101 @@ const StyledContainer = Container.extend`
   }
 `;
 
+const Results = ({ locationIn, resultsData }) => {
+  if (locationIn !== 'test') {
+    return null;
+  }
+
+  return (
+    <Container padding="default">
+      <h4>Results</h4>
+      <RatioAsEmojis ratio={resultsData.ratio} />
+      <p>
+        Tests passing:
+        {' '}
+        {resultsData.passedTests}
+        {' / '}
+        {resultsData.tests.length}
+      </p>
+    </Container>
+  );
+};
+
+Results.propTypes = {
+  ...localPropTypes,
+  ...resultsProps,
+};
+
+const NavigationTitle = ({ children }) => (
+  <Container padding="default">
+    <h4>Navigation</h4>
+    {children}
+  </Container>
+);
+
+NavigationTitle.defaultProps = {
+  children: null,
+};
+
+NavigationTitle.propTypes = {
+  children: PropTypes.element,
+};
+
 const Navigation = (props) => {
   const {
+    locationIn,
     testByPathName,
     tests,
   } = props;
 
-  if (!testByPathName) {
-    return (
-      <Fragment>
-        <h4>Tests available</h4>
-        <Nav>
-          {map(tests, test => (
-            <li key={test.path}>
-              <Link href={`/intro${test.path}`} to={`/intro${test.path}`}>
-                {test.name}
-              </Link>
-            </li>
-          ))}
-        </Nav>
-      </Fragment>
-    );
+  switch (locationIn) {
+    case 'home':
+      return (
+        <NavigationTitle>
+          <Nav>
+            <List>
+              {map(tests, test => (
+                <li key={test.path}>
+                  <Link href={`/intro${test.path}`} to={`/intro${test.path}`}>
+                    {test.name}
+                  </Link>
+                </li>
+              ))}
+            </List>
+          </Nav>
+        </NavigationTitle>
+      );
+    case 'instructions':
+      return (
+        <NavigationTitle>
+          <Link
+            margin="small"
+            to={testByPathName.path}
+            href={testByPathName.path}
+          >
+            Test
+          </Link>
+        </NavigationTitle>
+      );
+    case 'test':
+      return (
+        <NavigationTitle>
+          <Link
+            margin="small"
+            to={`/intro${testByPathName.path}`}
+            href={`/intro${testByPathName.path}`}
+          >
+            Instructions
+          </Link>
+        </NavigationTitle>
+      );
+    default:
+      return null;
   }
-
-  if (isPathnameInInstructions(props)) {
-    return (
-      <Link
-        margin="small"
-        to={testByPathName.path}
-        href={testByPathName.path}
-      >
-        Test
-      </Link>
-    );
-  }
-
-  return (
-    <Link
-      margin="small"
-      to={`/intro${testByPathName.path}`}
-      href={`/intro${testByPathName.path}`}
-    >
-      Instructions
-    </Link>
-  );
 };
 
 Navigation.propTypes = {
+  ...localPropTypes,
   ...testPropTypes,
   ...testsPropTypes,
 };
@@ -94,9 +152,10 @@ class SideBar extends Component {
     const {
       setActiveTest,
       testByPathName,
+      locationIn,
     } = props;
 
-    if (testByPathName) {
+    if (locationIn === 'instructions' || locationIn === 'test') {
       setActiveTest(testByPathName.name);
     }
   }
@@ -117,27 +176,48 @@ class SideBar extends Component {
 
   render() {
     return (
-      <StyledContainer padding="default">
+      <StyledContainer>
         <Navigation {...this.props} />
+        <Results {...this.props} />
       </StyledContainer>
     );
   }
 }
 
 SideBar.propTypes = {
+  ...localPropTypes,
   ...withRouterProps,
 };
 
 const mapStateToProps = (state, props) => ({
   testByPathName: storeTests.selectors.getTestByPathName(state)(getTestPathFromLocation(props)),
   tests: storeTests.selectors.getTests(state),
+  resultsData: codeResults.selectors.getData(state),
 });
 
 const mapDispatchToProps = {
   setActiveTest: storeActiveTest.actions.setActiveTest,
 };
 
+const getLocationStatus = (props) => {
+  if (!props.testByPathName) {
+    return 'home';
+  }
+
+  if (isPathnameInInstructions(props)) {
+    return 'instructions';
+  }
+
+  return 'test';
+};
+
+const mappedProps = mapProps(props => ({
+  ...props,
+  locationIn: getLocationStatus(props),
+}));
+
 export default compose(
   withRouter,
   connect(mapStateToProps, mapDispatchToProps),
+  mappedProps,
 )(SideBar);
